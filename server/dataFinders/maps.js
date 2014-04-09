05efac24a2efe301aecc71719c1bfe1c868588c2
ash47 <ash.fourtyseven@gmail.com>
2014-04-09 17:46:55 +1000

    Updated building scrapper

--- server/dataFinders/maps.js
+++ server/dataFinders/maps.js
@@ -5,38 +5,98 @@ var http = require('http');
 // Jquery stuff
 var cheerio = require('cheerio');
 
-// Load any old buildings
-var buildings = {};
-if(fs.existsSync('./buildings.json')) {
-    buildings = require('./buildings.json');
-}
-
-// Map Location
-var mapLoc = 'http://maps.unimelb.edu.au/parkville/building/';
+// Padding to remove when detecting fields
+var fieldPadding = '<strong>:</strong> ';
 
 /*
-Class to cache buildings
+Building Cacher Class
 */
-function buildingCacher(name, href) {
-    this.name = name;
-    this.href = href;
+function buildingCacher(rootName, href, callback) {
+    // Store the root directory
+    this.toCache = [];
+
+    // Where all the data will be stored
+    this.data = {};
+
+    // We have to cache the root
+    this.toCache.push({
+        name: rootName,
+        href: href,
+        store: this.data
+    });
+
+    // Store callback
+    this.callback = callback;
+
+    // Start Caching
+    this.cache();
 }
 
-// Caches a building
-buildingCacher.prototype.cache = function(callback) {
-    console.log('Caching: '+this.name);
+// This caches the next building in the list
+buildingCacher.prototype.cache = function() {
+    // Reference to this local object
+    var thisMap = this;
 
-    var content = '';
+    // Ensure there is something to cache
+    if(thisMap.toCache.length <= 0) return;
+
+    // Tell user how much is left to cache
+    console.log('\nLeft to cache = '+thisMap.toCache.length);
+
+    // Grab what we need to cache
+    var toCache = thisMap.toCache.pop();
+
+    // Tell the user what we're caching
+    console.log('Caching: '+toCache.name+' ('+toCache.href+')');
 
     // Request the page (this will have a link to rooms)
-    http.get(this.href, function(res) {
+    var content = '';
+    http.get(toCache.href, function(res) {
         res.on('data', function(chunk) {
             content += chunk;
         }).on('end', function() {
             // Load the page into cheerio
             var $ = cheerio.load(content);
 
-            console.log('Got a link back:');
+            // Contains fields for this building
+            var fields = {};
+
+            // See if there is any information about this building
+            $('.col-6.first .col-4.first ul li').each(function() {
+                // Used to grab data
+                var link = $(this);
+
+                // Grab the name of the field
+                var fieldName = $('strong', link).html();
+                var fieldValue = '';
+
+                // Make sure we found a fieldName
+                if(fieldName) {
+                    // Remove the colon from the field name
+                    fieldName = fieldName.substring(0, fieldName.length-1);
+
+                    // Grab the value of the field
+                    fieldValue = link.html().substring(fieldName.length+fieldPadding.length);
+                } else {
+                    var subLink = $('a', link);
+                    if(!subLink) return;
+
+                    // Fieldname is now the text that appear on the href
+                    fieldName = subLink.html();
+
+                    // Fieldvalue is now the href part
+                    fieldValue = subLink.attr('href');
+                }
+
+                // Store info
+                fields[fieldName] = fieldValue;
+
+                // Tell the user the field's we found
+                console.log(fieldName+' - '+fieldValue);
+            });
+
+            // List of children
+            fields.children = {};
 
             $('.within ul li a').each(function() {
                 // Grab useful stuff
@@ -44,79 +104,64 @@ buildingCacher.prototype.cache = function(callback) {
                 var name = link.html();
                 var href = link.attr('href');
 
-                // Tell the user
-                console.log('DATA: '+name+' - '+href);
+                // Check if it's a PDF
+                if(href.substring(href.length-4) == '.pdf') {
+                    // Store the link to the pdf
+                    fields[name] = href;
+
+                    // Don't try to cache it
+                    return;
+                }
+
+                // Add it to the list of things to cache
+                thisMap.toCache.push({
+                    name: name,
+                    href: href,
+                    store: fields.children
+                });
             });
 
-            // Check if there is a callback
-            if(callback) {
-                // Run the callback
-                callback();
+            // Store into parent
+            toCache.store[toCache.name] = fields;
+
+            // Check if there is anything else to cache
+            if(thisMap.toCache.length > 0) {
+                // Yes, continue caching
+                thisMap.cache();
+            } else {
+                // No, run callback if it exists
+                if(thisMap.callback) thisMap.callback(thisMap.data);
             }
         });
     });
 }
 
-// This function update the map list
-function buildMaps() {
-    // Tell the user
-    console.log('Updating maps...');
-
-    // List of buildings left to check
-    this.toBuild = [];
-}
-
-// Start caching
-buildMaps.prototype.startCaching = function() {
-    console.log('\n\nHERE\n\n');
-    console.log(this.toBuild);
-
-    // Alias of this
-    var thisMap = this;
-
-    // Request the page
-    var content = '';
-    http.get(mapLoc, function(res) {
-        // Grab the data:
-        res.on('data', function (chunk) {
-            content += chunk;
-        }).on('end', function() {
-            // Load the page into cheerio
-            var $ = cheerio.load(content);
-
-            $('.within ul a').each(function() {
-                var link = $(this);
-                var name = link.html();
-                var href = link.attr('href');
-                //console.log(name+' - '+href);
-
-                // Store that this building needs caching
-                thisMap.toBuild.push(new buildingCacher(name, href));
-            });
-
-            // Continue Caching
-            thisMap.cacheNext();
-        });
-    }).on('error', function(err) {
-        throw err;
+/*
+//Redmond Barry test
+new buildingCacher('Redmond Barry Building', 'http://maps.unimelb.edu.au/parkville/building/115#.U0TgUvmSzHU', function(data) {
+    console.log('Done caching, saving...');
+    fs.writeFile('maps.json', JSON.stringify(data), function (err) {
+        if (err) throw err;
+        console.log('maps.json saved!');
     });
-}
+});*/
 
-// Cache the next building
-buildMaps.prototype.cacheNext = function() {
-    var thisMap = this;
+// Root Test
+new buildingCacher('root', 'http://maps.unimelb.edu.au/parkville/building/', function(data) {
+    console.log('Done caching, saving...');
+    fs.writeFile('maps.json', JSON.stringify(data), function (err) {
+        if (err) throw err;
+        console.log('maps.json saved!');
+    });
+});
 
-    if(this.toBuild.length > 0) {
-        // Grab the first thing that needs caching
-        var toCache = this.toBuild.pop();
-
-        // Cache it
-        toCache.cache(function() {
-            thisMap.cacheNext();
-        });
-    }
-}
-
-// Build the maps
-var cacher = new buildMaps();
-cacher.startCaching();
+/*
+// ERC Test Case
+new buildingCacher('ERC', 'http://maps.unimelb.edu.au/parkville/building/171/ERC', function(data) {
+    console.log('Done caching, saving...');
+    fs.writeFile('maps.json', JSON.stringify(data), function (err) {
+        if (err) throw err;
+        console.log('maps.json saved!');
+    });
+});
+*/

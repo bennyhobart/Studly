5dc6caa5f47857c6d8acb4f9ced6b1df64b3c35f
Renlord <renlord.yang@gmail.com>
2014-05-05 13:01:09 +1000

    socket, http APIs foundations are laid.

---
+++ server/app/README.md
@@ -0,0 +1,16 @@
+## Introduction
+
+This is the server side of things for Studly.
+
+Entry Point: app.js
+
+Configurations, Routings are currently all done within app.js. 
+DB Configs are also done in app.js
+
+## Instructions
+1. bower install
+2. npm install
+
+You should be able to instantiate the server now by running:
+
+node app.js
---
+++ server/app/socketserver.js
@@ -0,0 +1,30 @@
+var passportsocketio = require('passport.socketio');
+
+module.exports = function(io, cookieParser, key, secret, sessionStore) {
+    io.set('authorization', passportSocketIo.authorize({
+        cookieParser: cookieParser,
+        key:    key,
+        secret: secret,
+        store:  sessionStore,
+        success:onAuthorizeSuccess,
+        fail:   onAuthorizeFail
+    }))
+
+    function onAuthorizeSuccess(data, accept){
+        console.log('successful connection to socket.io!');
+        accept(null, true);
+    }
+
+    function onAuthorizeFail(data, message, error, accept) {
+        if(error) throw new Error(message);
+        console.log('failed connection to socket.io:', message);
+        accept(null, false);
+    }
+
+    io.sockets.on('connection', function(socket) {
+        /* FILL IN SOCKET LISTENERS HERE */
+        socket.on("hi", function(data, fn) {
+            fn('hi back!');
+        })
+    }
+}

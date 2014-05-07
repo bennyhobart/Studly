9f7ea39a8dcb3c41d0767d8be4823a5daaf15593
Benedict Hobart <hobart.benedict@gmail.com>
2014-05-07 12:51:58 +1000

    fixed some errors in api-reference.txt, updated styles of class page, added font awesome as a dependency, cleaned up some css stuff, needs to be refactored... added classes session subject and timetable services that will provide access to the apis

--- client/app/scripts/services/user.js
+++ client/app/scripts/services/user.js
@@ -1,17 +1,6 @@
 'use strict';
 
 angular.module('studlyApp')
-  .factory('User', function () {
-    // Service logic
-    // ...
-
-    // Public API here
-    return {
-      getUsername: function() {
-
-      },
-      getPassword: function () {
-        return meaningOfLife;
-      },
-    };
+  .factory('user', ['$resource' , function ($resource)) {
+    return $resource('/api/user');
   });

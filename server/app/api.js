19e413305dc3a9e1a303c19111bef32f0e0e560e
Benedict Hobart <hobart.benedict@gmail.com>
2014-05-09 16:38:28 +1000

    Updated api requirements, added some route controllers for server api's

--- server/app/api.js
+++ server/app/api.js
@@ -1,7 +1,12 @@
 module.exports = function(app, passport) {
-  
+  session = require('controllers/session');
+  _class = require('controllers/class');
+  user = require('controllers/user');
+  subject = require('controllers/subject');
+
+
   /* STANDARD ROUTING */
-  app.get("/", function(req, res) {
+  app.get('/', function(req, res) {
     if (req.isAuthenticated()) {
       res.sendfile('client/app/views/main.html');
     } else {
@@ -9,18 +14,48 @@ module.exports = function(app, passport) {
     }
   });
 
+  app.route('/api/session')
+    .get(session.me)
+    .post(session.login)
+    .delete(session.logout);
+
+  app.route('/api/user')
+    .get(user.getPublicProfile)
+    .post(user.register)
+    .delete(user.deregister)
+    .put(user.updatePassword);
+
+  app.route('/api/class')
+    .get(_class.getWeeklyClass)
+    .post(_class.addThread)
+    .delete(_class.deleteThread)
+    .put(_class.attendClass);
+
+
+
+  app.route('/api/subject/:id')
+    .get(subject.getSubject);
+
+  app.route('/api/subject')
+    .get(subject.search);
+
+  app.route('/api', function (req, req) {
+    req.send(404, 'No API Found');
+  });
+
+
   app.get('/logout', function(req, res) {
     req.logout();
     res.redirect('/');
   });
 
-  app.post("/login", passport.authenticate('local', {
-      successRedirect : "/",
-      failureRedirect : "/",
+  app.post('/login', passport.authenticate('local', {
+      successRedirect : '/',
+      failureRedirect : '/',
       failureFlash: true
     })
   );
 
   /* API */
-  
+
 }
---
+++ server/app/controllers/user.js
@@ -0,0 +1,16 @@
+
+exports.getPublicProfile = function (req, res) {
+
+};
+
+exports.register = function (req, res) {
+
+};
+
+exports.deregister = function (req, res) {
+
+};
+
+exports.updatePassword = function (req, res) {
+
+};

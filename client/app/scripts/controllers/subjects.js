df5ef817a152846e175525e3b028fe0fb59fdb5f
AlgoRyan <contact.hodgman.r@gmail.com>
2014-05-13 22:36:43 +1000

    Changes to date on the subjects page
    -Still working on fitting the page with the rest of the app

--- client/app/scripts/controllers/subjects.js
+++ client/app/scripts/controllers/subjects.js
@@ -1,23 +1,48 @@
 'use strict';
 
-angular.module('studlyApp')
-  .controller('SubjectsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    $scope.subjects = [];
-    var exampleSubject = {
-    	name: "Artificial Intelligence",
-    	code: "COMP30050",
-    	classes: [],
-    };
-    for (var i = 0; i<10; ++i) {
-    	$scope.subjects.push(exampleSubject);
-    }
+var app = angular.module('studlyApp');
 
-    $scope.userAlert = function() {
-      alert("_This will search for the subject code, then display the subject name and a prompt to confirm enrolment_");
-    };
-  });
+app.controller('SubjectsCtrl', function ($scope) { 
+  var AI = {
+    name: "Artificial Intelligence",
+	  code: "COMP30050",
+  };
+  var CS = {
+    name: "Computer Systems",
+    code: "COMP30123",
+  };
+  var Phil = {
+    name: "Philosophy",
+    code: "PHIL20057",
+  };
+  var UNIB = {
+    name: "African Drumming",
+    code: "UNIB10003",
+  };
+  var CHEM = {
+    name: "Practical Chemistry",
+    code: "CHEM20006",
+  };
+
+  $scope.enrolled = [CS, AI, Phil];
+
+  $scope.subjects = [CS, AI, Phil, UNIB, CHEM];
+
+  $scope.userAlert = function() {
+    alert("_This will search for the subject code, then display the subject name and a prompt to confirm enrolment_");
+  };
+});
+
+// app.directive('resize', function ($scope, $window) {
+//   // Function to handle changes to style classes based on window width
+//   var w = angular.element($window);
+
+//   w.bind('resize', function() {
+//     if (w.width() < 768) {
+//       $scope.search-results.max-height = 150px;
+//     };
+//     if (w.width() >= 768) {
+//       $scope.search-results.max-height = 400px;
+//     };
+//   });
+// });

3f5f49c2fbf2e1bcb0c2b5595d1ea49c8580491f
Benedict Hobart <hobart.benedict@gmail.com>
2014-05-13 10:15:53 +1000

    updated login styles and some bootstrap variables, fixed navbar and removed footeR

--- client/app/scripts/directives/navbar.js
+++ client/app/scripts/directives/navbar.js
@@ -1,12 +1,15 @@
 'use strict';
 
 angular.module('studlyApp')
-  .directive('navbar', function () {
+  .directive('navbar', [ '$location', function ($location) {
     return {
       templateUrl: 'views/navbar.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
         // element.text('this is the navbar directive');
+        scope.isActive = function(route) {
+            return route === $location.path();
+        };
       }
     };
-  });
+  }]);

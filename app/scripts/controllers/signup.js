4fb77e8fdbf2d1aaa55a4497d0c4b4d40cf9222f
Benedict Hobart <hobart.benedict@gmail.com>
2014-05-20 13:58:28 +1000

    added simple functionality to signup page

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('studlyApp')
-  .controller('SignupCtrl', function ($scope, $http) {
-
+  .controller('SignupCtrl', function ($scope, $location, $http, Auth) {
+    $scope.submitForm = function () {
+        if ($scope.password!=$scope.passwordConfirm) {
+            return;
+        }
+        Auth.createUser({
+            email: $scope.email,
+            password: $scope.password,
+            passwordRepeated: $scope.passwordRepeated
+        }, function (err) {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            $location.url('/timetable');
+        });
+    };
   });

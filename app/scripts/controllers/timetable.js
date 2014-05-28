4d9149ae70b76d70c64d53db0af15917b17d570f
Benedict Hobart <hobart.benedict@gmail.com>
2014-05-29 08:37:35 +1000

    small changes

--- app/scripts/controllers/timetable.js
+++ app/scripts/controllers/timetable.js
@@ -2,12 +2,12 @@
 
 
 angular.module('studlyApp')
-  .controller('TimetableCtrl', function ($scope, $location, Timetable, Class) {
+  .controller('TimetableCtrl', function ($scope, $location, Timetable, Class, $rootScope) {
 
 
   /*Class.update({
     classId: '302030230203',
-    attendedFlag: true 
+    attendedFlag: true
   }).$promise.then(
   //success
   function (data) {
@@ -140,7 +140,7 @@ angular.module('studlyApp')
 
   $scope.updateSql();
 
-    /*Timetable.get({
+    Timetable.get({
       date: $scope.sqlQueryDate($scope.thisWeekObject)
     }).$promise.then(
     //success
@@ -153,9 +153,10 @@ angular.module('studlyApp')
       console.log('Error getting data 2.');
     });
 
-    $scope.goToClass = function (classId) {
-      $location.url('/class/' + classId);
-    };*/
+    $scope.goToClass = function (weeklyClassId) {
+      $location.url('/class');
+      $rootScope.weeklyClassId = weeklyClassId;
+    };
 
 
   $scope.formatTime = function(time) {
@@ -173,7 +174,7 @@ angular.module('studlyApp')
 		 } else {
 		   	amPM = " AM";
 		 }
-		
+
 		 return hour + ":" + time.toString().slice(-2);
   };
 

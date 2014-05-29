20bad2ef1ed6a609ae88fb57504b6e958b2bf35a
AlgoRyan <contact.hodgman.r@gmail.com>
2014-05-29 15:19:12 +1000

    Subjects calling to the server
    Working on:
    -Displaying class times to enrol in (subject page)
    -Enrolling in a subject (subject page)
    -Getting the enrolled subjects to display in the subjects page

--- app/scripts/controllers/subject.js
+++ app/scripts/controllers/subject.js
@@ -1,6 +1,65 @@
 'use strict';
 
 angular.module('studlyApp')
-.controller('SubjectCtrl', function ($scope, $rootScope) {
-	var manageID = $rootScope.manageID;
+.controller('SubjectCtrl', function ($scope, $rootScope, Subject) {
+	var subjectID;
+	$scope.subjectName = "Placeholder";
+
+	$scope.init = function () { 
+		subjectID = $rootScope.manageID;
+		Subject.getData({id: subjectID}).
+			$promise.then(function (data) {
+				// Success
+	                console.log(data);
+	                $scope.subjectName = data.subjectName;
+	                $scope.subjectCode = data.subjectCode;
+	                $scope.semesterID = data.semesterID;
+				    $scope.classList = data.class;
+				    angular.forEach($scope.classList, function(index) {
+				    	this.push(index.classTimes);
+				    }, $scope.classTimes);
+	            }, function (data) {
+	            // Failure
+	                console.log(data);
+	        	}
+	        );
+	}
+
+	Subject.get({
+      query: $scope.queryField
+    }).$promise.then(
+      //success
+      function (data) {
+        console.log(data)
+        angular.forEach(data.subjects, function() {
+
+        })
+        $scope.subjects = data.subjects;
+        console.log($scope.subjects);
+      },
+      //failure
+      function (data) {
+        console.log(data);
+      }
+    );
+
+	$scope.enrol = function () { 
+		Subject.enrol({
+			subjectID: subjectID 
+		}).$promise.then(function (data) {
+            console.log(data);
+        }, function (data) {
+            console.log(data);
+        });
+	}
+
+	$scope.withdraw = function () { 
+		Subject.withdraw({
+			subjectID: subjectID 
+		}).$promise.then(function (data) {
+            console.log(data);
+        }, function (data) {
+            console.log(data);
+        });
+	}
 });
--- app/scripts/services/subject.js
+++ app/scripts/services/subject.js
@@ -2,5 +2,9 @@
  
 angular.module('studlyApp')
   .factory('Subject', ['$resource' , function ($resource) {
-    return $resource('/api/subject/:id', {id: "@id"})
+    return $resource('/api/subject/:id', {id: '@id'}, {
+    	getData: { method: 'GET' },
+    	withdraw: { method: 'DELETE' },
+		enrol: { method: 'POST' },
+    })
 }]);
